c5187e2babc846235a76d03454e4f76018fe61e8
Tpearlz <omoboyowa.tolulope@gmail.com>
2025-06-10 22:54:21 -0700

    Update Join_Queries.sql
    Join_Query_with_Using

--- Join_Queries.sql
+++ Join_Queries.sql
@@ -28,3 +28,10 @@ INNER JOIN prime_ministers AS p2
 ON p1.country = p2.country
 INNER JOIN monarchs AS p3
 ON p1.continent = p3.continent;
+
+--Join Example with USING
+SELECT p1.country, p1.continent,  
+       president, prime_minister 
+FROM prime_ministers AS p1 
+INNER JOIN presidents AS p2 
+USING(country);
