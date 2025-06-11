3df4a2126d9d29589eec9686431c3d36ad9e12a0
Tpearlz <omoboyowa.tolulope@gmail.com>
2025-06-10 22:33:08 -0700

    Update Join_Queries.sql
    Using Aliased Tables

--- Join_Queries.sql
+++ Join_Queries.sql
@@ -7,3 +7,10 @@ prime_minister, president
 FROM presidents
 INNER JOIN prime_ministers
 ON presidents.country = prime_ministers.country;
+
+Inner Join Using Aliased Tables
+
+SELECT p2.country, p2.continent, prime_minister,president
+FROM presidents AS p1
+INNER JOIN prime_ministers AS p2
+ON p1.country = p2.country;
