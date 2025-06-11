f0d87e5d390333d3eb916d64a3b1e97bcaea9b05
Tpearlz <omoboyowa.tolulope@gmail.com>
2025-06-10 22:52:32 -0700

    Update Join_Queries.sql
    Multiple_joins

--- Join_Queries.sql
+++ Join_Queries.sql
@@ -1,6 +1,6 @@
 Select * FROM monarchs;
 
-Inner Join Using ON
+--Inner Join Using ON
 
 SELECT prime_ministers.country, prime_ministers.continent,
 prime_minister, president
@@ -8,15 +8,23 @@ FROM presidents
 INNER JOIN prime_ministers
 ON presidents.country = prime_ministers.country;
 
-Inner Join Using Aliased Tables
+--Inner Join Using Aliased Tables
 
 SELECT p2.country, p2.continent, prime_minister,president
 FROM presidents AS p1
 INNER JOIN prime_ministers AS p2
 ON p1.country = p2.country;
 
-Inner Join using USING
+--Inner Join using USING
 SELECT p2.country, p2.continent, prime_minister, president
 FROM presidents AS p1
 INNER JOIN prime_ministers AS p2
 USING(country);
+
+--Mutiple inner Joins
+SELECT *
+FROM presidents AS p1
+INNER JOIN prime_ministers AS p2
+ON p1.country = p2.country
+INNER JOIN monarchs AS p3
+ON p1.continent = p3.continent;
