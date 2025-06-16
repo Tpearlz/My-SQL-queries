e5c6d45a05d67929211c1ff3070b2fb88e2a69c9
Tpearlz <omoboyowa.tolulope@gmail.com>
2025-06-16 10:32:12 -0700

    Update Interview_Questions.sql
    Averaging

--- Interview_Questions.sql
+++ Interview_Questions.sql
@@ -2,3 +2,9 @@ SELECT Extract(Day from post_date) AS DAY, count(post_text) As Total_Posts
 FROM facebook_posts
 GROUP BY DAY
 ORDER By DAY ASC;
+
+select Customer_id, count(customer_id) AS customer_order, 
+Avg (amount) AS order_amount
+from postmates_orders
+GROUP BY customer_id
+ORDER BY order_amount ASC;
